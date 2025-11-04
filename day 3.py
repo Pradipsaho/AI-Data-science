14b8d65fb49eb7d44c64a3d157748e8ceb2c51a5
pradip kumar <pradipkumar@pradips-MacBook-Air.local>
2025-11-04 18:23:41 +0530

    adding a new practice set

---
+++ day 3.py
@@ -0,0 +1,7 @@
+'''🏠 Homework (Practice for Day 3)
+Add a new column Result — value should be:
+"Pass" if grade ≠ Fail, else "Fail".
+Find average marks per grade using groupby("Grade")["Average"].mean().
+Sort the DataFrame by "Average" descending and print top 2 students.'''
+
+import pandas as pd
